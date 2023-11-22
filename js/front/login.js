fcafcd861855e943d01411031b8605ff8fea7fa3
Damian.Garcia <Damian.Garcia@toshibagcs.com>
2023-11-22 10:30:31 -0600

    feat: declare variables for html elements in login window.

---
+++ js/front/login.js
@@ -0,0 +1,5 @@
+//Maybe an import goes here, need to understand NodeJs and electron first tho
+//HTML elements
+var name = document.getElementById ('textName');
+var pwd = document.getElementById ('textPwd');
+var button = document.getElementById('logInB');
